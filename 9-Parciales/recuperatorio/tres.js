7c59ff19085f3cf97e61a76c97b647552af4305e
lautaromedeiros98 <36174125+lautaromedeiros98@users.noreply.github.com>
2018-02-06 20:48:03 -0300

    Add files via upload
    ejercicio parcial recuperatorio tres

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultado;
+	var resultadoDos
+
+
+	precio1=document.getElementById('precioUno').value;
+	precio2=document.getElementById('precioDos').value;
+	precio3=document.getElementById('precioTres').value;
+
+    precio1=parseInt(precio1);
+    precio2=parseInt(precio2);
+    precio3=parseInt(precio3);
+
+    resultado=precio1+precio2+precio3;
+    resultadoDos=precio1+precio1+precio3/3;
+
+    alert(resultado);
+    alert(resultadoDos);
+
+
+
+
 
 }
