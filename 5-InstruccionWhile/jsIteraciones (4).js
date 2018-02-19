4c6ea9f2f962de9b692bf6b574949ff45096ff96
lautaromedeiros98 <36174125+lautaromedeiros98@users.noreply.github.com>
2018-02-19 20:22:28 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,10 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
-
+	while(numero<0 || numero>9)
+	{
+		alert("Vuelve a ingresar un numero valido");
+		numero= prompt("Ingrese el numero nuevamente");
+	}
 
 }//FIN DE LA FUNCIÓN
