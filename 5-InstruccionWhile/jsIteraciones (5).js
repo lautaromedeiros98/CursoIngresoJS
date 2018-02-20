dc4933e296721cc05a21aa5b91b9244f8533bd50
lautaromedeiros98 <36174125+lautaromedeiros98@users.noreply.github.com>
2018-02-19 22:11:54 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,23 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+	var sexo = prompt("ingrese f ó m .");
+	sexo== "f" || "m";
+		while (sexo != "f" && sexo != "m" )
+		{
+			alert("Sexo incorrecto");
+			sexo= prompt("ingrese nuevamente.");
+		}
+
+			if (sexo= "f")
+			{
+				document.getElementById('Sexo').value=("El sexo es femenino "  );
+			}
+			if (sexo = "m")
+			{
+				document.getElementById('Sexo').value=("El sexo es masculino " );
+			}
 
 
-
-document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,9 @@
 function Mostrar()
 {
-
 	var contador=0;
 	var acumulador=0;
+	var numeroActual=0;
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
-
-}//FIN DE LA FUNCIÓN
+}
