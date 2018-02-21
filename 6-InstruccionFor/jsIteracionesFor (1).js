17e672ffddff6010cdd767acd9d53dcbbc7ca6d2
lautaromedeiros98 <36174125+lautaromedeiros98@users.noreply.github.com>
2018-02-20 21:58:03 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,87 @@
 function Mostrar()
 {
+	var contadorDePositivos=0;
+	var contadorDeNegativos=0;
+	var contadorDeCero=0;
+	var contadorDePar=0;
+	var contadorDeImpar=0;
+	var contador50=0;
+	
+	for(contador=-100;contador<100;contador++)
+	{
+		if(contador>0)
+		{
+			contadorDePositivos++;
+		}
+		else
+		{
+			if(contador>0)
+			{
+				contadorDeNegativos++;
+			}
+			else
+			{
+				contadorDeCero++;
+			}
+		}
+
+	}
+	if(contador%2==0 && contador !=)
+	{
+		contadorDePar++;
+	}
+	else
+	{
+		if(contador!=0)
+		{
+			contadorDeImpar++;
+		}
+	}
+	switch(contador)
+		{
+			case 50:
+				contador50++;
+				break;
+		}
 
 }
+	document.write("positivos"+contadorDePositivos);
+	document.write("negativos"+contadorDeNegativos);
+	document.write("cero"+contadorDeCero);
+	document.write("par"+contadorDePar);
+	document.write("impar"+contadorDeImpar);
+	document.write("de 50:" +contador50);
+
+	
+	
+}
+
+
+
+
+
+//for(contador=0;contador<10;contador++)
+
+
+
+
+
+
+
+
+/*for(;;)
+	{
+		contador++;
+		document.write("<br>"+contador);
+		if (contador==10)
+		{
+			document.write("<br>break"+contador);
+			break;
+
+		}
+		if(contador==5)
+		{
+			continue;
+		}
+		document.write("<br>continue "+contador);
+	*/
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,97 @@
 function Mostrar()
 {
+	var numeroRandom;
+	var contador0=0;
+	var contador1=0;
+	var contador2=0;
+	var contador3=0;
+	var contador4=0;
+	var contador5=0;
+	var contador6=0;
+	var contador7=0;
+	var contador8=0;
+	var contador9=0;
+	var porcen0;
+	var iteraciones=1000;
+
+	for(contador=0;contador<iteraciones;contador++)
+	{
+		numeroRandom= Math.floor((Math.random() * 10) );
+		//document.write(numeroRandom);
+		switch(numeroRandom)
+		{
+			case 0:
+				contador0++;
+			case 1:
+				contador1++;
+			case 2:
+				contador2++;
+			case 3:
+				contador3++;
+				contador3++;
+				contador++;
+				break;
+			case 4:
+				contador4++;
+			case 5:
+				contador5++;
+			case 6:
+			porcen6=(contador6/(iteraciones))*100;
+
+
+			if(porcen6>7)
+			{
+				continue;
+			}
+			else
+			{
+				contador--;
+				contador6++;
+			}
+			break;
+				
+			case 7:
+				contador7++;
+			case 8:
+				contador8++;
+			case 9:
+				contador9++;
+				break;
+
+		}//switch
+		
+	}//for
+
+	porcen0=(contador0/(contador+1))*100;
+	porcen0=Math.round(porcen0);
+	document.write("<br>cantidad 0: "+contador0 +" %: " + porcen0);
+	porcen1=(contador1/(contador+1))*100;
+	porcen1=Math.round(porcen1); 
+	document.write("<br>cantidad 1: "+contador1 +" %: " + porcen1);
+	porcen2=(contador2/(contador+1))*100;
+	porcen2=Math.round(porcen2);
+	document.write("<br>cantidad 2: "+contador2 +" %: " + porcen2);
+	porcen3=(contador3/(contador+1))*100;
+	porcen3=Math.round(porcen3);
+	document.write("<br>cantidad 3: "+contador3 +" %: " + porcen3);
+	porcen4=(contador4/(contador+1))*100; 
+	porcen4=Math.round(porcen4);
+	document.write("<br>cantidad 4: "+contador4 +" %: " + porcen4);
+	porcen5=(contador5/(contador+1))*100;
+	porcen5=Math.round(porcen5);
+	document.write("<br>cantidad 5: "+contador5 +" %: " + porcen5);
+	porcen6=(contador6/(contador+1))*100;
+	porcen6=Math.round(porcen6);
+	document.write("<br>cantidad 6: "+contador6 +" %: " + porcen6);
+	porcen7=(contador7/(contador+1))*100;
+	porcen7=Math.round(porcen7);
+	document.write("<br>cantidad 7: "+contador7 +" %: " + porcen7);
+	porcen8=(contador8/(contador+1))*100;
+	porcen8=Math.round(porcen8);
+	document.write("<br>cantidad 8: "+contador8 +" %: " + porcen8);
+	porcen9=(contador9/(contador+1))*100;
+	porcen9=Math.round(porcen9);
+	document.write("<br>cantidad 9: "+contador9 +" %: " + porcen9);
 
 
 }
